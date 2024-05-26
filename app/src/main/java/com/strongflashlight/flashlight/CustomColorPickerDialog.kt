8690d2ea82888ee43d5e44a498a268a25fd356f6
Younghyun SEO <168096909+Lucky-SeoYounghyun@users.noreply.github.com>
2024-05-26 19:58:22 +0900

    Update CustomColorPickerDialog.kt
    License

--- app/src/main/java/com/strongflashlight/flashlight/CustomColorPickerDialog.kt
+++ app/src/main/java/com/strongflashlight/flashlight/CustomColorPickerDialog.kt
@@ -27,32 +27,51 @@ class CustomColorPickerDialog(context: Context, private val callback: ColorPicke
     }
 
     private fun setupViews() {
-        // 보안처리되었습니다
+        colorPickerView = findViewById<ColorPickerView>(R.id.colorPickerView)
+        brightnessSlider = findViewById<BrightnessSlideBar>(R.id.brightnessSlider)
+
+        setupColorButton(R.id.colorWhite, 0xFFFFFFFF.toInt())
+        setupColorButton(R.id.colorRed, 0xFFFF0000.toInt())
+        setupColorButton(R.id.colorBlue, 0xFF0000FF.toInt())
+        setupColorButton(R.id.colorBlack, 0xFF000000.toInt())
+
+        colorPickerView.attachBrightnessSlider(brightnessSlider)
+        adjustColorPickerSize(colorPickerView)
 
     }
     private fun setupListeners() {
         val buttonSelect = findViewById<Button>(R.id.button_select)
         val buttonCancel = findViewById<Button>(R.id.button_cancel)
 
-        // 보안처리되었습니다
+        buttonSelect.setOnClickListener {
+            val color = colorPickerView.color
+            callback.onColorSelected(color)  // 'callback' 사용
+            dismiss()
+        }
 
+        buttonCancel.setOnClickListener {
+            dismiss()
+        }
     }
 
 
     private fun initializeColorPicker(initialColor: Int) {
-        // 보안처리되었습니다
-
+        val initialColor = loadInitialColor()
+        if (initialColor != -1) {
+            findViewById<ColorPickerView>(R.id.colorPickerView).setInitialColor(initialColor)
+        }
     }
 
     private fun loadInitialColor(): Int {
-        // 보안처리되었습니다
-
+        val sharedPref = context.getSharedPreferences("ColorPicker", Context.MODE_PRIVATE)
+        return sharedPref.getInt("lastColor", -1)
     }
 
     private fun setupColorButton(buttonId: Int, color: Int) {
         val button = findViewById<Button>(buttonId)
-        // 보안처리되었습니다
-
+        button.setOnClickListener {
+            findViewById<ColorPickerView>(R.id.colorPickerView).setInitialColor(color)
+        }
     }
     private fun adjustColorPickerSize(colorPickerView: ColorPickerView) {
         val displayMetrics = DisplayMetrics()
@@ -62,7 +81,26 @@ class CustomColorPickerDialog(context: Context, private val callback: ColorPicke
         val minDimension = minOf(width, height)
 
         val size = (minDimension * 2) / 3
-        // 보안처리되었습니다
-
+        colorPickerView.layoutParams.width = size
+        colorPickerView.layoutParams.height = size
+        colorPickerView.requestLayout()
+        colorPickerView.invalidate()
+        Log.d("ColorPickerSize", "Width: $size, Height: $size")
     }
 }
+
+/*
+Copyright 2017 skydoves (Jaewoong Eum)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
